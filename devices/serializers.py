be701d513daac851fa5cdf4e9e38cdc82b096d74
maheshnikange <maheshnikange7@gmail.com>
2025-12-21 10:54:24 +0530

    first commit

--- devices/serializers.py
+++ devices/serializers.py
@@ -1,7 +1,17 @@
-from rest_framework import serializers
-from .models import Device
+from rest_framework import serializers, views
+from rest_framework.response import Response
+from rest_framework import status
 
-class DeviceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Device
-        fields = "__all__"
+class DeviceSerializer(serializers.Serializer):
+    device_id = serializers.CharField(max_length=50)
+    ram_gb = serializers.IntegerField(required=True)
+    status = serializers.CharField(max_length=20)
+
+class HeartbeatAPIView(views.APIView):
+    def post(self, request):
+        serializer = DeviceSerializer(data=request.data)
+        if serializer.is_valid():
+            # Save device data
+            return Response({"status": "ok"})
+        else:
+            return Response({"status": "error", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
